52b34c12f81e9b0c349968529f461b057f78a8c9
BryGuy4Life <btn2189@gmail.com>
2017-10-18 10:32:38 -0500

    first commit

---
+++ scripts.js
@@ -0,0 +1,45 @@
+const q1 = {
+    question: 'String of quesiton to ask',
+    a1:  {
+        answer: 'String value of text answer to show user',
+        correct: true
+    
+    },
+    a2:  {
+        answer: 'String value of text answer to show user',
+        correct: false
+    },
+    a3:  {
+        answer: 'String value of text answer to show user',
+        correct: false
+    },
+    a4:  {
+        answer: 'String value of text answer to show user',
+        correct: false
+    }
+    
+};
+const q2 = {
+    question: 'String of quesiton to ask',
+    answersThatAreFalse: [
+        {
+            answer: 'String value of text answer to show user',
+            correct: true
+
+        },
+        {
+            answer: 'String value of text answer to show user',
+            correct: false
+        },
+        {
+            answer: 'String value of text answer to show user',
+            correct: false
+        },
+        {
+            answer: 'String value of text answer to show user',
+            correct: false
+        }
+    ]
+};
+
+const questionArray = [q1, q2];
